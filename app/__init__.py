41f523334ff6ce2ad23f4c446e17ecdb68debb83
Lucaslllll <lucascosta12367@gmail.com>
2021-04-05 22:13:16 -0300

    adicionando banco de dados para futuras modificações nesse site estático

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////storage.db'
+db = SQLAlchemy(app)
 
 from app.controllers import default # normal esse tipo de importação no flask
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -6,4 +6,5 @@ from flask import render_template
 def index():
 	return render_template('index.html')
 
+	
 
